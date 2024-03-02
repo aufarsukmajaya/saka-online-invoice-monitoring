c3b62945620d7bf345a52553f7713cf06586af00
Aufar Sukmajaya <aufarsukmajaya@gmail.com>
2024-03-02 15:06:54 +0000

    prometheus

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -3,5 +3,5 @@ docker run -d \
 --name=prometheus \
 -v /home/itadmin/go/src/saka-online-invoice-monitoring/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
 -v /home/itadmin/database/prometheus/:/prometheus \
-prom/prometheus:$(PROMETHEUS_VERSION) \
+prom/prometheus:${PROMETHEUS_VERSION} \
 --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention.time=3d
