c7eb220da6699053baecd47c546f352bf3ed52e9
Aufar Sukmajaya <aufarsukmajaya@gmail.com>
2024-03-02 15:04:56 +0000

    prometheus

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 .PHONY: run-prometheus
 
+export PROMETHEUS_VERSION ?= v2.50.1
+
 run-prometheus:
 	-docker rm --force prometheus
 	sh prometheus/prometheus.sh
--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -2,6 +2,6 @@ docker run -d \
 --network=einvoice \
 --name=prometheus \
 -v /home/itadmin/go/src/saka-online-invoice-monitoring/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
--v /home/itadmin/database/prometheus:/prometheus \
-prom/prometheus:v2.50.1 \
+-v /home/itadmin/database/prometheus/:/prometheus \
+prom/prometheus:$(PROMETHEUS_VERSION) \
 --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention.time=3d
