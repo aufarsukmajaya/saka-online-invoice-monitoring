b993d7021b3de990fe6eeeb980149b327172ceea
DESKTOP-UCTBAA2\aufar <aufarsukmajaya@gmail.com>
2024-03-22 23:59:10 +0700

    collector

--- postgres-exporter/postgres-exporter.sh
+++ postgres-exporter/postgres-exporter.sh
@@ -1,4 +1,4 @@
 docker run -d --name=postgres-exporter \
 --network=${DOCKER_NETWORK} \
 -e DATA_SOURCE_NAME=${POSTGRES_EXPORTER_SOURCE} \
-quay.io/prometheuscommunity/postgres-exporter:${POSTGRES_EXPORTER} --no-collector.replication --no-collector.replication_slot --collector.long_running_transactions --collector.stat_statements
+quay.io/prometheuscommunity/postgres-exporter:${POSTGRES_EXPORTER} --no-collector.replication --no-collector.replication_slot --no-collector.wal --collector.long_running_transactions --collector.stat_statements
