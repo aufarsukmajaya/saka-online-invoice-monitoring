67f71fb30b22492f9ca39eea9d3b31523241a5e9
DESKTOP-UCTBAA2\aufar <aufarsukmajaya@gmail.com>
2024-03-22 23:22:41 +0700

    einv

--- postgres-exporter/postgres-exporter.sh
+++ postgres-exporter/postgres-exporter.sh
@@ -1,4 +1,4 @@
 docker run -d --name=postgres-exporter \
---network=vagrant \
+--network=einvoice \
 -e DATA_SOURCE_NAME=${POSTGRES_EXPORTER_SOURCE} \
 quay.io/prometheuscommunity/postgres-exporter:${POSTGRES_EXPORTER} --no-collector.replication --no-collector.replication_slot --collector.long_running_transactions --collector.stat_statements
